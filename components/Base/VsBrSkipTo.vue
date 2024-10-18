bb395788e7f41cd475cf24686ec7a95ffaf9e7e5
mprosser <Michael.Prosser@visitscotland.com>
2024-10-18 14:11:26 +0100

    feat(skip to): add SkipTo to starter, fix linting

---
+++ components/Base/VsBrSkipTo.vue
@@ -0,0 +1,23 @@
+<template>
+    <VsSkipTo
+        :skip-to-text="configStore.getLabel('skip-to', 'skip-to.label')"
+        :has-search-link="false"
+    >
+        <template #main-menu-text>
+            {{ configStore.getLabel('skip-to', 'skip-to.main-menu') }}
+        </template>
+        <template #main-content-text>
+            {{ configStore.getLabel('skip-to', 'skip-to.main-content') }}
+        </template>
+        <template #footer-text>
+            {{ configStore.getLabel('skip-to', 'skip-to.footer') }}
+        </template>
+    </VsSkipTo>
+</template>
+
+<script lang="ts" setup>
+import { VsSkipTo } from '@visitscotland/component-library/components';
+import useConfigStore from '~/stores/configStore.ts';
+
+const configStore = useConfigStore();
+</script>
