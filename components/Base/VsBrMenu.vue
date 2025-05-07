0ea999cbe123ba7040f9998df5760a74513e6b76
Michael Prosser <michael.prosser@visitscotland.com>
2025-05-07 13:32:02 +0100

    feat(article): integrate article module

--- components/Base/VsBrMenu.vue
+++ components/Base/VsBrMenu.vue
@@ -1,75 +1,73 @@
 <!-- eslint-disable vue/valid-v-slot -->
 <template>
-    <div>
-        <div class="vs-sticky-nav" :class="{ 'has-edit-button': page.isPreview() }">
-            <VsBrSkipTo />
-            <header>
-                <VsGlobalMenu
-                    :active-site="configStore.activeSite === 'be'
-                        ? 'https://businessevents.visitscotland.com/'
-                        : 'https://www.visitscotland.com/'"
-                >
-                    <template #third-menu-item>
-                        <VsGlobalMenuLanguage
-                            :language="configStore.locale"
-                            :language-label="configStore.getLabel('navigation.static', 'universal.language')"
-                        >
-                            <VsGlobalMenuLanguageItem
-                                v-for="(language, index) in localisedUrls"
-                                :key="index"
-                                :language-link="language.url"
-                                :language-name="language.displayName"
-                                :language="language.locale.language"
-                            />
-                        </VsGlobalMenuLanguage>
-                    </template>
-                </VsGlobalMenu>
-
-                <BrManageMenuButton :menu="menuData" />
-
-                <VsMeganav
-                    href="/"
-                    :menu-toggle-alt-text="configStore.getLabel('navigation.static', 'meganav-toggle-btn-alt-text')"
-                    :search-button-text="configStore.getLabel('search', 'search')"
-                    :search-label-text="configStore.getLabel('search', 'search-label')"
-                    :search-clear-button-text="configStore.getLabel('search', 'clear-form')"
-                    :search-close-button-text="configStore.getLabel('search', 'close-form')"
-                    :logo-alt-text="configStore.getLabel('navigation.static', 'meganav.logo-alt-text')"
-                    :is-static="true"
-                >
-                    <template #mega-nav-top-menu-items>
-                        <VsBrMegaNav
-                            :links="menuItems"
+    <div class="vs-sticky-nav" :class="{ 'has-edit-button': page.isPreview() }">
+        <VsBrSkipTo />
+        <header>
+            <VsGlobalMenu
+                :active-site="configStore.activeSite === 'be'
+                    ? 'https://businessevents.visitscotland.com/'
+                    : 'https://www.visitscotland.com/'"
+            >
+                <template #third-menu-item>
+                    <VsGlobalMenuLanguage
+                        :language="configStore.locale"
+                        :language-label="configStore.getLabel('navigation.static', 'universal.language')"
+                    >
+                        <VsGlobalMenuLanguageItem
+                            v-for="(language, index) in localisedUrls"
+                            :key="index"
+                            :language-link="language.url"
+                            :language-name="language.displayName"
+                            :language="language.locale.language"
                         />
-                    </template>
+                    </VsGlobalMenuLanguage>
+                </template>
+            </VsGlobalMenu>
 
-                    <template #mega-nav-accordion-items>
-                        <VsBrAccordionNav
-                            :links="menuItems"
-                        />
-                    </template>
-                </VsMeganav>
-            </header>
-        </div>
+            <BrManageMenuButton :menu="menuData" />
 
-        <VsBanner
-            v-if="banner"
-            :close-btn-text="configStore.getLabel('essentials.global', 'close')"
-        >
-            <template #banner-text>
-                <div v-html="banner.copy.value" />
-            </template>
+            <VsMeganav
+                href="/"
+                :menu-toggle-alt-text="configStore.getLabel('navigation.static', 'meganav-toggle-btn-alt-text')"
+                :search-button-text="configStore.getLabel('search', 'search')"
+                :search-label-text="configStore.getLabel('search', 'search-label')"
+                :search-clear-button-text="configStore.getLabel('search', 'clear-form')"
+                :search-close-button-text="configStore.getLabel('search', 'close-form')"
+                :logo-alt-text="configStore.getLabel('navigation.static', 'meganav.logo-alt-text')"
+                :is-static="true"
+            >
+                <template #mega-nav-top-menu-items>
+                    <VsBrMegaNav
+                        :links="menuItems"
+                    />
+                </template>
 
-            <template #banner-cta>
-                <VsLink
-                    :href="banner.ctaLink.link"
-                    :type="banner.ctaLink.type"
-                >
-                    {{ banner.ctaLink.label }}
-                </VsLink>
-            </template>
-        </VsBanner>
+                <template #mega-nav-accordion-items>
+                    <VsBrAccordionNav
+                        :links="menuItems"
+                    />
+                </template>
+            </VsMeganav>
+        </header>
     </div>
+
+    <VsBanner
+        v-if="banner"
+        :close-btn-text="configStore.getLabel('essentials.global', 'close')"
+    >
+        <template #banner-text>
+            <div v-html="banner.copy.value" />
+        </template>
+
+        <template #banner-cta>
+            <VsLink
+                :href="banner.ctaLink.link"
+                :type="banner.ctaLink.type"
+            >
+                {{ banner.ctaLink.label }}
+            </VsLink>
+        </template>
+    </VsBanner>
 </template>
 
 <script lang="ts" setup>
