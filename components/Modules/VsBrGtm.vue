4270dc8bdeacb8b68dc69a035557715ef4db2b1d
mprosser <Michael.Prosser@visitscotland.com>
2026-01-30 12:26:00 +0000

    fix(gtm): Don't load gtm in ssr

--- components/Modules/VsBrGtm.vue
+++ components/Modules/VsBrGtm.vue
@@ -15,7 +15,7 @@
 import useConfigStore from '~/stores/configStore.ts';
 
 /* eslint no-undef: 0 */
-import { inject } from 'vue';
+import { inject, onMounted } from 'vue';
 
 import type { Page } from '@bloomreach/spa-sdk';
 
@@ -99,23 +99,25 @@ const attachCivicEvents = (counter = 1) => {
     }
 };
 
-if (id && !isPreviewMode) {
-    useHead({
-        script: [
-            `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '${queryString}';f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${id}');`,
-        ],
-    });
+onMounted(() => {
+    if (id && !isPreviewMode) {
+        useHead({
+            script: [
+                `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '${queryString}';f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${id}');`,
+            ],
+        });
 
-    attachCivicEvents();
-}
+        attachCivicEvents();
+    }
 
-if (isPreviewMode && window) {
-    window.bypassCookiesRequired = true;
-    window.bypassCookiesLoaded = true;
-}
+    if (isPreviewMode && window) {
+        window.bypassCookiesRequired = true;
+        window.bypassCookiesLoaded = true;
+    }
+});
 
 </script>
