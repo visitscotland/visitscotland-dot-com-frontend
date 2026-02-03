2bf5846dce7f4f59f36b9c175422d60000a66823
mprosser <Michael.Prosser@visitscotland.com>
2026-02-03 14:01:41 +0000

    fix(megalinks): formatlink on link list cta

--- components/Modules/VsBrListLinksModule.vue
+++ components/Modules/VsBrListLinksModule.vue
@@ -7,7 +7,7 @@
         :cookie-link-text="configStore.getLabel('essentials.global', 'cookie.link-message')"
         :no-cookies-message="configStore.getLabel('video', 'video.no-cookies')"
         :no-js-message="configStore.getLabel('video', 'video.no-js')"
-        :button-link="module.cta ? module.cta.link : null"
+        :button-link="module.cta ? formatLink(module.cta.link) : null"
     >
         <template
             v-if="module.introduction"
