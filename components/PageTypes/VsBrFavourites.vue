66b47dc13a3d1a917c362fcb100a0053685f3397
Mark Mitchell <mark.mitchell@visitscotland.com>
2026-03-10 14:10:39 +0000

    cardgroup row-gap and properties file

--- components/PageTypes/VsBrFavourites.vue
+++ components/PageTypes/VsBrFavourites.vue
@@ -14,6 +14,7 @@
                     <VsCardGroup
                         variant="grid"
                         :cards-per-row="4"
+                        class="row-gap-lg-500"
                     >
                         <TransitionGroup name="fade">
                             <VsCard
