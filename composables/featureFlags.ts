85ef6e393e754ac44e2579d6e2640c9521f3787c
jhaddrill <james.haddrill@visitscotland.com>
2026-03-24 11:49:10 +0000

    Remove authentication middleware

--- composables/featureFlags.ts
+++ composables/featureFlags.ts
@@ -1,4 +1,6 @@
-// get feature flags from the backend and return them
-const featureFlagsData = await $fetch('/api/frontend/aws/flag-authentication');
+// this will return feature flag values from the AppCongig agent
+// for now, it's returning an empty object which will ensure that flags are not activated
+// this file will be updated when the AppConfig agent is implemented at server level.
+const featureFlagsData = {};
 
 export default featureFlagsData;
