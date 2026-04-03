2c5ae8dc05625e179b79865364c145eaf531c353
Mark Mitchell <7934920+markup-mitchell@users.noreply.github.com>
2026-04-03 13:16:08 +0100

    Update mitt.client.ts
    Coderabbit duplicated some syntax. deleted.

--- plugins/mitt.client.ts
+++ plugins/mitt.client.ts
@@ -1,15 +1,11 @@
 import mitt from 'mitt';
 
 export default defineNuxtPlugin(() => {
-export default defineNuxtPlugin(() => {
     const EVENT_BUS = mitt();
 
     return {
         provide: {
             eventBus: EVENT_BUS,
-        },
-    };
-});
         },
     };
 });
