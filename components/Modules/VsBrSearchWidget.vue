a8b77b7e3c4910340a35f858f509bb610326b687
chesterroberts-vs <chester.roberts@visitscotland.com>
2026-04-23 15:18:03 +0100

    fix(vsbrsearchwidget): missing check on section header lede

--- components/Modules/VsBrSearchWidget.vue
+++ components/Modules/VsBrSearchWidget.vue
@@ -3,7 +3,7 @@
         <VsBrSectionHeader
             v-if="module?.title && !isSiteSearchPage"
             :heading="module?.title"
-            :lede="module.introduction.value"
+            :lede="module?.introduction.value"
         />
 
         <VsBrSearchInput
