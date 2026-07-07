4d0737d203e00ffedaffa81dbba087010dacc869
mprosser <Michael.Prosser@visitscotland.com>
2026-07-07 11:08:40 +0100

    fix(breadcrumb): ensure normal breadcrumb loads in ssr context

--- components/Modules/VsBrBreadcrumb.vue
+++ components/Modules/VsBrBreadcrumb.vue
@@ -41,51 +41,51 @@ let breadcrumb = [];
 let isHome = false;
 let definedBreadcrumb = [];
 
+if (page) {
+    const pageContent : any = page.getContent(page.model.root);
+    const pageModels : any = pageContent.models;
+
+    if (pageModels) {
+        breadcrumb = pageModels.breadcrumb.items;
+        isHome = pageModels.isHome;
+        definedBreadcrumb = breadcrumb || [];
+    }
+}
+
 // On client mount, process breadcrumb and generate schema
 onMounted(() => {
-    if (page) {
-        const pageContent : any = page.getContent(page.model.root);
-        const pageModels : any = pageContent.models;
-
-        if (pageModels) {
-            breadcrumb = pageModels.breadcrumb.items;
-            isHome = pageModels.isHome;
-            definedBreadcrumb = breadcrumb || [];
-
-            // Construct root URL (always absolute on client)
-            rootUrl = window.location.origin;
-            if (configStore.langString) {
-                rootUrl = `${rootUrl}/${configStore.langString}`;
-            }
-
-            let itemList : any[] = [
-                {
-                    '@type': 'ListItem',
-                    position: 1,
-                    item: {
-                        '@id': `${ rootUrl }/`,
-                        name: configStore.getLabel('essentials.global', 'home'),
-                    },
-                },
-            ];
-
-            for (let x = 0; x < definedBreadcrumb.length; x++) {
-                itemList = itemList.concat({
-                    '@type': 'ListItem',
-                    position: x + 2,
-                    item: {
-                        '@id': `${ rootUrl }${ definedBreadcrumb[x].link.href }`,
-                        name: definedBreadcrumb[x].title,
-                    },
-                });
-            }
-
-            useJsonld({
-                '@context': 'https://schema.org',
-                '@type': 'BreadcrumbList',
-                itemListElement: itemList,
-            });
-        }
+    // Construct root URL (always absolute on client)
+    rootUrl = window.location.origin;
+    if (configStore.langString) {
+        rootUrl = `${rootUrl}/${configStore.langString}`;
     }
+
+    let itemList : any[] = [
+        {
+            '@type': 'ListItem',
+            position: 1,
+            item: {
+                '@id': `${ rootUrl }/`,
+                name: configStore.getLabel('essentials.global', 'home'),
+            },
+        },
+    ];
+
+    for (let x = 0; x < definedBreadcrumb.length; x++) {
+        itemList = itemList.concat({
+            '@type': 'ListItem',
+            position: x + 2,
+            item: {
+                '@id': `${ rootUrl }${ definedBreadcrumb[x].link.href }`,
+                name: definedBreadcrumb[x].title,
+            },
+        });
+    }
+
+    useJsonld({
+        '@context': 'https://schema.org',
+        '@type': 'BreadcrumbList',
+        itemListElement: itemList,
+    });
 });
 </script>
