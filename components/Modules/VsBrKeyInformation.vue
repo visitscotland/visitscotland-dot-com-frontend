09f6ad2928423ccc8a644af9f52583ebb1dd7181
Mike Hart <mike.hart@visitscotland.com>
2026-09-07 11:57:33 +0100

    init key information module

---
+++ components/Modules/VsBrKeyInformation.vue
@@ -0,0 +1,22 @@
+<template>
+    <VsPanel>
+        <VsHeading
+            level="2"
+            heading-style="heading-s"
+        >
+            Key Information
+        </VsHeading>
+
+        <VsBody>
+            Hello, Key Information Panel! This is a placeholder for content.
+        </VsBody>
+    </VsPanel>
+</template>
+
+<script setup lang="ts">
+import {
+    VsBody,
+    VsHeading,
+    VsPanel,
+} from '@visitscotland/component-library/components';
+</script>
