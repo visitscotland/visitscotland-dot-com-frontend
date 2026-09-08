3401b24279d8597fe510be703c3f48704951ea0b
Mike Hart <mike.hart@visitscotland.com>
2026-09-08 12:01:21 +0100

    Add some initial markup structure

--- components/Modules/VsBrKeyInformation.vue
+++ components/Modules/VsBrKeyInformation.vue
@@ -4,11 +4,26 @@
             level="2"
             heading-style="heading-s"
         >
-            Key Information
+            {{ module.title || 'Key Information' }}
         </VsHeading>
 
         <VsBody>
-            Hello, Key Information Panel! This is a placeholder for content.
+            <div>
+                <ul class="list-unstyled d-flex flex-column m-0 p-0 gap-075">
+                    <li v-for="highlight in module.highlights" :key="highlight.category.key" class="d-flex gap-050">
+                        <VsIcon
+                            icon="vs-icon-control-dismiss"
+                            aria-hidden="true" />
+                        <span>
+                            <span>{{ highlight.category.label }}:</span> {{ highlight.copy }}
+                        </span>
+                    </li>
+                </ul>
+                <VsButton>{{  module.cta.label }}</VsButton>
+            </div>
+            <div>
+                <!-- TODO Placeholder for map inclusion VS-1835 -->
+            </div>
         </VsBody>
     </VsPanel>
 </template>
@@ -16,7 +31,9 @@
 <script setup lang="ts">
 import {
     VsBody,
+    VsButton,
     VsHeading,
+    VsIcon,
     VsPanel,
 } from '@visitscotland/component-library/components';
 
@@ -37,4 +54,8 @@ interface KeyInformationModule {
     },
     highlights: KeyInformationHighlight[]
 }
+
+defineProps<{
+    module: KeyInformationModule
+}>();
 </script>
