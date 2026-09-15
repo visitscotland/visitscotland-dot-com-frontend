6dfada999b11d52d42de92d1aba25f98f5eb6fcd
mjpvs <Michael.Prosser@visitscotland.com>
2026-09-15 10:29:38 +0100

    feat(module wrapper): switch travel info to new template

--- components/Modules/VsBrTravelInformationModule.vue
+++ components/Modules/VsBrTravelInformationModule.vue
@@ -1,43 +1,54 @@
 <template>
-    <VsModuleWrapper theme="neutral">
-        <template #vs-module-wrapper-heading>
-            {{ module.title }}
-        </template>
-
-        <template #vs-module-wrapper-intro v-if="module.copy">
-            <VsBrRichText
-                :input-content="module.copy.value"
-            />
-        </template>
+    <section class="vs-br-travel-information-module">
+        <VsBrSectionHeader
+            :heading="module.title"
+            :lede="module.copy?.value"
+        />
 
         <VsContainer>
             <VsRow>
                 <VsCol cols="12" sm="10" offset-sm="1">
                     <VsTabs>
-                        <VsBrTravelInformationTab :tab="module.gettingTo" :tab-index="0" />
-                        <VsBrTravelInformationTab :tab="module.gettingAround" :tab-index="1" />
+                        <VsBrTravelInformationTab
+                            v-if="module.gettingTo"
+                            :tab="module.gettingTo"
+                            :tab-index="0"
+                        />
+                        <VsBrTravelInformationTab
+                            v-if="module.gettingAround"
+                            :tab="module.gettingAround"
+                            :tab-index="1"
+                        />
                     </VsTabs>
                 </VsCol>
             </VsRow>
         </VsContainer>
-    </VsModuleWrapper>
+    </section>
 </template>
 
 <script lang="ts" setup>
-
-import VsBrRichText from '~/components/Modules/VsBrRichText.vue';
-
 import {
-    VsModuleWrapper,
     VsContainer,
     VsRow,
     VsCol,
     VsTabs,
 } from '@visitscotland/component-library/components';
 
+import VsBrSectionHeader from './VsBrSectionHeader.vue';
 import VsBrTravelInformationTab from './VsBrTravelInformationTab.vue';
 
 const props = defineProps<{ module: object }>();
 const module: any = props.module;
 
 </script>
+
+<style lang="scss" scoped>
+.vs-br-travel-information-module {
+    padding: 3rem 0;
+    background-color: #E5E5F0;
+
+    @media (min-width: 576px) {
+        padding: 4.5rem 0 6rem;
+    }
+}
+</style>
