14809c3fe34557c2ef1d5e38f30d0e410423dadf
Mike Hart <mike.hart@visitscotland.com>
2026-09-15 12:10:38 +0100

    correct icon names, fix styles

--- components/Modules/VsBrKeyInformationPanel.vue
+++ components/Modules/VsBrKeyInformationPanel.vue
@@ -12,21 +12,27 @@
             <div>
                 <ul class="key-information__highlights list-unstyled d-flex flex-column m-0 p-0 gap-075 mt-150 text-tertiary">
                     <li v-for="highlight in module.highlights" :key="highlight.category.key" class="d-flex gap-050">
-                        <span class="key-information__icon-wrapper d-flex justify-content-center">
+                        <span class="highlight__icon-wrapper d-flex justify-content-center">
                             <VsIcon
                                 :icon="getIconName(highlight.category.key)"
                                 variant="tertiary"
                                 aria-hidden="true"
                             />
                         </span>
-                        <div v-html="highlight.copy.value" />
+                        <span class="highlight__text">
+                            <span>{{ highlight.category.value }}: </span>
+                            <span
+                                class="highlight__copy"
+                                v-html="highlight.copy.value"
+                            />
+                        </span>
                     </li>
                 </ul>
                 <!-- Desktop button -->
                 <VsButton
                     v-if="module.cta"
                     :href="module.cta.link"
-                    class="mt-150 d-none d-md-block"
+                    class="key-information__cta-desktop mt-150 d-none d-md-block"
                 >
                     {{ module.cta.label }}
                 </VsButton>
@@ -34,16 +40,15 @@
             <div>
                 <div>
                     <!-- TODO Placeholder for map inclusion VS-1835 -->
-                    map
                 </div>
                 <div>
                     <!-- Mobile button -->
                     <VsButton
                         v-if="module.cta"
                         :href="module.cta.link"
-                        class="mt-150 d-md-none"
+                        class="key-information__cta-mobile mt-150 d-md-none"
                     >
-                        {{ module.cta.label }}
+                        {{ module.cta.label }} mobile
                     </VsButton>
                 </div>
             </div>
@@ -89,8 +94,19 @@ defineProps<{
 <style lang="css" scoped>
     .key-information__highlights {
         color: #535396; /* Text tertiary */
-        .key-information__icon-wrapper {
+        .highlight__icon-wrapper {
             min-width: 1.6rem;
         }
+        /* :deep uses vue scoped css for dynamically rendered HTML */
+        .highlight__copy :deep(p) {
+            display: inline;
+            margin: 0;
+        }
+    }
+    .key-information__cta-desktop {
+        width: fit-content;
+    }
+    .key-information__cta-mobile {
+        width: 100%;
     }
 </style>
